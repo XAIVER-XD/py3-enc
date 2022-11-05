eb31b705083e3854221d67e26c35ccd745e5d178
XAIVER-XD <116884025+XAIVER-XD@users.noreply.github.com>
2022-11-05 17:29:57 +0600

    Add files via upload

---
+++ py3-enc.py
@@ -0,0 +1,31 @@
+import os, platform
+ os.system('xdg-open https://www.facebook.com/100011755265711')
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from XAI64  import void
+ 
+        void()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from ET32 import void
+ 
+ 
+        void()
+ 
